745c6cd0ad855ea2a4d292c7e96508c332cf5007
DoDoGaMaRu <nm09090@gmail.com>
2023-05-23 19:04:05 +0900

    mainpage 추가

---
+++ src/mainpage/MainPage.js
@@ -0,0 +1,14 @@
+import Header from '../nav/main-top-header/Header';
+import Content from './component/Content';
+import './MainPage.css';
+
+function MainPage() {
+  return (
+    <div>
+      <Header />
+      <Content />
+    </div>
+  );
+}
+
+export default MainPage;
---
+++ src/mainpage/component/Content.js
@@ -0,0 +1,12 @@
+import './Content.css'
+
+function Content() {
+    // TODO <div data-v-42d86f9b="" class="layout-content-scroll"> 구현
+    return (
+        <div class="content">
+            
+        </div>
+    )
+}
+
+export default Content;
