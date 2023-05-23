36270a2f3d17269fc6050fc1e8ff5f180c079aa3
DoDoGaMaRu <nm09090@gmail.com>
2023-05-23 19:04:53 +0900

    네비게이션 헤더 추가

---
+++ src/nav/main-top-header/Header.js
@@ -0,0 +1,68 @@
+import './Header.css'
+import menu from './img/menu_main.png'
+import logo from './img/logo_main.png'
+import pizza from './img/pizza_main.png'
+import plane from './img/plane_main.png'
+
+function Header() {
+    return (
+        <div id="main-tap-header">
+            <TapTop />
+            <TapSlide />
+            <TapNav />
+        </div>
+    )
+}
+
+function TapTop() {
+    let middleItems = ['피자', '스페셜반반피자', '세트', '사이드', '하프엔하프', '멤버십·제휴할인', '이벤트']
+    let rightItems = ['마이페이지', '회원가입', '로그인']
+
+    return (
+        <div class="main-tap-top">
+            <div class="tap-top-left">
+                <img class="icon-menu" src={menu}></img>
+                <img class="icon-logo" src={logo}></img>
+            </div>
+            <div class="tap-top-middle">
+                {middleItems.map(e=>TopMiddleText(e))}
+            </div>
+            <div class="tap-top-right">
+                <div class="top-right-layout">
+                    <div class="top-right-text-layout">
+                        {rightItems.map(e=>TopRightText(e))}
+                    </div>
+                    <img class="icon-pizza" src={pizza}></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function TopMiddleText(text) {
+    return (
+        <span class="top-middle-text">{text}</span>
+    )
+}
+
+function TopRightText(text) {
+    return (
+        <span class="top-right-text">{text}</span>
+    )
+}
+
+
+function TapSlide() {
+    return(
+        <div class="main-tap-slide">
+            <img class="icon-plane" src={plane}></img>
+        </div>
+    )
+}
+
+
+function TapNav() {
+
+}
+
+export default Header;
