4f85923a73e9670796c517a278d2bbc2f82397aa
daehyun03 <kimdeahyeon5445@gmail.com>
2023-06-09 08:20:01 +0900

    장바구니, 리뷰 페이지 완

--- src/index.js
+++ src/index.js
@@ -3,18 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import MainPage from './mainpage/MainPage'
 import reportWebVitals from './reportWebVitals';
-import Basket from "./basket/Basket";
-import {Provider} from "react-redux";
-import store from "./store"
-import Review from "./review/Review";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-      <Provider store={store}>
-        <Review />
-      </Provider>
-  </React.StrictMode>
+    <React.StrictMode>
+        <MainPage />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
